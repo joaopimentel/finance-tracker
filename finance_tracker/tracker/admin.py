d1e3619b748a88255b516c734dd6465edb996165
Joao Pimentel <mail@joaopimentel.net>
2015-07-19 17:52:59 +0100

    nice SecurityDataPoint modeladmin with nice list_display

--- finance_tracker/tracker/admin.py
+++ finance_tracker/tracker/admin.py
@@ -3,5 +3,14 @@ from django.contrib import admin
 from .models import Security, SecurityDataPoint
 
 
+class SecurityDataPointAdmin(admin.ModelAdmin):
+    def time_format(self, obj):
+        return obj.timestamp.strftime("%Y-%m-%d (%H:%M:%S)")
+    time_format.admin_order_field = 'timestamp'
+    time_format.short_description = 'Timestamp'
+
+    list_display = ('time_format', 'security', 'unit_value', 'currency')
+
+
 admin.site.register(Security)
-admin.site.register(SecurityDataPoint)
+admin.site.register(SecurityDataPoint, SecurityDataPointAdmin)
