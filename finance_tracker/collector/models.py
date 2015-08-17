866f52f718407877760bc0675ed980b7acb4ac6e
Joao Pimentel <mail@joaopimentel.net>
2015-08-17 22:16:50 +0100

    add SpecificXMLDataSource with raw data retrieve method

--- finance_tracker/collector/models.py
+++ finance_tracker/collector/models.py
@@ -1,5 +1,6 @@
 from __future__ import unicode_literals
 
+from lxml import html
 import requests
 from django.db import models
 
@@ -30,3 +31,30 @@ class ExternalFileDataSource(BaseDataSource):
 
     def data_to_datapoints(self):
         raise NotImplementedError
+
+
+class SpecificXMLDataSource(ExternalFileDataSource):
+    """
+    Expects a XML/HTML with a table like:
+        <table>
+            <caption></caption>
+            <tr><th>DATA<th><th>PRECO<th></tr>
+            <tr><td>date</td><td>price</td></tr>
+            ...
+    And expects that:
+        - header row is Date, Price;
+        - date is in dd-mm-yyyy;
+        - price has a ',' for decimal point;
+    """
+    def iter_raw_data(self):
+        content = self.read_file()
+        tree = html.fromstring(content)
+        for tr in tree.iter('tr'):
+            vals = list(tr.iter('td'))
+            if len(vals) == 0:
+                # Should be the header row, skip
+                continue
+            yield vals[0].text_content(), vals[1].text_content()
+
+
+
