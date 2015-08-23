3499974b3f5b2fefe55364cd66aa518d9d7f9237
Joao Pimentel <mail@joaopimentel.net>
2015-08-23 21:04:00 +0100

    register Portfolio and Position on admin site

--- finance_tracker/tracker/admin.py
+++ finance_tracker/tracker/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 
-from .models import Security, SecurityDataPoint
+from .models import (
+    Portfolio,
+    Position,
+    Security,
+    SecurityDataPoint,
+)
 
 
 class SecurityDataPointAdmin(admin.ModelAdmin):
@@ -14,3 +19,17 @@ class SecurityDataPointAdmin(admin.ModelAdmin):
 
 admin.site.register(Security)
 admin.site.register(SecurityDataPoint, SecurityDataPointAdmin)
+
+admin.site.register(Portfolio)
+
+
+class PositionAdmin(admin.ModelAdmin):
+    def time_format(self, obj):
+        return obj.timestamp.strftime("%Y-%m-%d (%H:%M:%S)")
+    time_format.admin_order_field = 'timestamp'
+    time_format.short_description = 'Timestamp'
+
+    list_display = ('portfolio', 'time_format', 'security', 'units')
+
+
+admin.site.register(Position, PositionAdmin)
