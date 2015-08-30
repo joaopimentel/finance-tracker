1378d4898abac49dcb629ce8eb0bedbc69814f10
Joao Pimentel <mail@joaopimentel.net>
2015-08-30 22:50:34 +0100

    add get_security_units_for_portfolio method and tests

--- finance_tracker/tracker/portfolio.py
+++ finance_tracker/tracker/portfolio.py
@@ -1,7 +1,15 @@
-from tracker.models import SecurityDataPoint
+from django.db.models import Sum
+
+from tracker.models import Position, SecurityDataPoint
 
 
 def get_position_last_value(position):
     dp = SecurityDataPoint.objects.filter(security=position.security).last()
     value = dp.unit_value * position.units
     return value
+
+
+def get_security_units_for_portfolio(portfolio, security):
+    agg = Position.objects.filter(portfolio=portfolio, security=security) \
+                          .aggregate(total_units=Sum('units'))
+    return agg['total_units']
--- finance_tracker/tracker/tests/test_portfolio.py
+++ finance_tracker/tracker/tests/test_portfolio.py
@@ -10,14 +10,17 @@ from tracker.models import (
     Portfolio,
     Position,
 )
-from tracker.portfolio import get_position_last_value
+from tracker.portfolio import (
+    get_position_last_value,
+    get_security_units_for_portfolio,
+)
 
 
 class PositionTest(TestCase):
 
     def setUp(self):
         self.sec = Security.objects.create(name='sec', isin='sec')
-        portfolio = Portfolio.objects.create(name='portfolio')
+        self.portfolio = Portfolio.objects.create(name='portfolio')
 
         timestamps = [
             datetime(2014, 10, 24, 9, 0, 0, tzinfo=timezone.utc),
@@ -33,17 +36,39 @@ class PositionTest(TestCase):
             SecurityDataPoint.objects.create(security=self.sec,
                                              timestamp=t,
                                              unit_value=v)
-        self.pos = Position(
+
+    def test_position_value(self):
+        pos = Position(
             security=self.sec,
             timestamp=datetime(2014, 10, 27, 9, 0, 0, tzinfo=timezone.utc),
             units=Decimal('1.0'),
-            portfolio=portfolio,
+            portfolio=self.portfolio,
         )
-
-    def test_position_value(self):
-        val = get_position_last_value(self.pos)
+        val = get_position_last_value(pos)
         self.assertEqual(val, Decimal('2.0'))
         # Change number of units for position
-        self.pos.units = Decimal('3.0')
-        val = get_position_last_value(self.pos)
+        pos.units = Decimal('3.0')
+        val = get_position_last_value(pos)
         self.assertEqual(val, Decimal('6.0'))
+
+    def test_total_units(self):
+        position_units = [
+            Decimal('1.0'),
+            Decimal('3.0'),
+            Decimal('-1.5'),
+            Decimal('-2.0'),
+        ]
+        timestamps = [
+            datetime(2014, 10, 24, 9, 0, 0, tzinfo=timezone.utc),
+            datetime(2014, 10, 27, 9, 0, 0, tzinfo=timezone.utc),
+            datetime(2014, 10, 28, 9, 0, 0, tzinfo=timezone.utc),
+            datetime(2014, 10, 29, 9, 0, 0, tzinfo=timezone.utc),
+        ]
+        [Position.objects.create(security=self.sec,
+                                 timestamp=t,
+                                 units=u,
+                                 portfolio=self.portfolio)
+            for u, t in zip(position_units, timestamps)]
+        self.assertEqual(get_security_units_for_portfolio(self.portfolio,
+                                                          self.sec),
+                         Decimal('0.5'))
